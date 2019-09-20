d9e5a537b5aae0a329d661061627157a0ce3ba9e
artinZareie <artin.zareie@yahoo.com>
2019-09-20 19:55:38 +0430

    add phpdoc

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Japh framework
+ * 
+ * Japh is a php framework that its goal is developing frontend and backend very easily.
+ * 
+ * @author Artin Zareie <artin.zareie@yahoo.com>
+ * @version 1.0.0-beta 
+ */
+
 require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'vendor', 'autoload.php']);
 
 (new App\Bootstrap\Bootstrap());
